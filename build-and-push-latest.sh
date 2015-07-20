f671b36805383d5ddccdc8c900a98cd4eba65e7a
Andy McKay <amckay@mozilla.com>
2015-07-20 14:05:36 -0700

    oops

--- build-and-push-latest.sh
+++ build-and-push-latest.sh
@@ -1,10 +1,19 @@
 # This builds every image, tags it as latest and then pushes it up to docker hub.
 
-baseimages=(mysql python27)
+baseimages=(mysql nodejs phantomjs python27)
 for i in "${baseimages[@]}"
 do
     echo 'Building and pushing' $i
     docker build -t mozillamarketplace/centos-$i-mkt:latest docker/base-images/$i
     docker push mozillamarketplace/centos-$i-mkt:latest
+    echo '... done.'
+done
+
+images=(mysql-service mysql-data nginx elasticsearch redis memcached)
+for i in "${images[@]}"
+do
+    echo 'Building and pushing' $i
+    docker build -t mozillamarketplace/$i:latest mkt/data/images/$i
+    docker push mozillamarketplace/$i:latest
     echo '... done.'
 done
