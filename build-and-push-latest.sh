4c777384fd3de2accd26865b9b1cd73b983beabd
Andy McKay <amckay@mozilla.com>
2015-07-20 13:43:08 -0700

    add in volume to the service

--- build-and-push-latest.sh
+++ build-and-push-latest.sh
@@ -1,19 +1,10 @@
 # This builds every image, tags it as latest and then pushes it up to docker hub.
 
-baseimages=(mysql nodejs phantomjs python27)
+baseimages=(mysql python27)
 for i in "${baseimages[@]}"
 do
     echo 'Building and pushing' $i
     docker build -t mozillamarketplace/centos-$i-mkt:latest docker/base-images/$i
     docker push mozillamarketplace/centos-$i-mkt:latest
-    echo '... done.'
-done
-
-images=(mysql-service mysql-data nginx elasticsearch redis memcached)
-for i in "${images[@]}"
-do
-    echo 'Building and pushing' $i
-    docker build -t mozillamarketplace/$i:latest docker/images/$i
-    docker push mozillamarketplace/$i:latest
     echo '... done.'
 done
